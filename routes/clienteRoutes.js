3948cc58202acaa5fa3f965de49b82debe84458a
Jonathan Saraiva <jonathan.saraiva15@gmail.com>
2025-04-07 21:10:46 -0300

    Codificação do back-end do projeto integrador (requisição get para obter dados dos clientes)

---
+++ routes/clienteRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const clienteController = require('../controllers/clienteController');
+
+//Defina a rota para obter os dados
+router.get('/clientes', clienteController.getDadosCliente);
+
+
+module.exports = router;
